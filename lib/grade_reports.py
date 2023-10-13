caa2bb08907e71a7bca8c7818d372f1fceab03d9
Cooper <55swimmer55@gmail.com>
2023-10-13 11:45:54 -0400

    Built a basic CLI template using Canvas tutorial

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -2,3 +2,20 @@
 
 if __name__ == '__main__':
     pass
+
+def create_grade_report(student_grades):
+    with open('./reports/grade_report.txt', 'a') as gr:
+        for grade in student_grades:
+            # add '\n' to write grades on separate lines
+            gr.write(grade + '\n')
+
+if __name__ == '__main__':
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        # end when no grade is entered
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
